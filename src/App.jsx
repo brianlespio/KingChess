e401d6fcd88d9dcd123ce5483634d6511a5b34e2
Chuck <86530032+brianlespio@users.noreply.github.com>
2025-06-13 15:18:03 +0200

    Add lobby and game styles and import in components

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Lobby from './Lobby';
+import Game from './Game';
+
+function App() {
+  return (
+    <>
+      <Lobby />
+      <Game />
+    </>
+  );
+}
+
+export default App;
